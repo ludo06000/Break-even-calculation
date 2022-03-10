c4e96b2427590fb3febc569330c86d492fe5f832
Ludo <lc06000@gmail.com>
2022-03-10 13:29:22 +0100

    commit graph test

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let calcBtn =  document.getElementById("calcBtn");
-let resetBtn = document.getElementById("resetBtn");
+var calcBtn =  document.getElementById("calcBtn");
+var resetBtn = document.getElementById("resetBtn");
+var graphContainer = document.getElementById("graphContainer");
 
 function breakEventPoint() {
 
@@ -9,9 +10,10 @@ function breakEventPoint() {
     let benefitView = document.getElementById("benefitView");
     let srView =  document.getElementById("srView");
     let variableCostView =  document.getElementById("variableCostView");
-
-
     
+
+
+    // calcul des données numériques 
     mscv = caht.value*(margeRate.value/100);
     benefit = mscv - cfAmount.value;
     sr = cfAmount.value/(margeRate.value/100);
@@ -19,6 +21,49 @@ function breakEventPoint() {
     variableCostView.value = caht.value - mscv;
     benefitView.value = benefit;
     srView.value = sr;
+
+    // Données Graphiques
+    //document.querySelector('.graphSr').clear();
+    //document.querySelector("graphSr").remove()
+
+    var canvasContainer = document.createElement('canvas');
+    canvasContainer.className = 'graphSr'
+    graphContainer.appendChild(canvasContainer);
+
+    let canvas = document.querySelector(".graphSr");
+    let context = canvas.getContext('2d');
+
+
+
+    var data = {
+        labels : ['0', caht.value*0.3333, caht.value*0.6666, caht.value, caht.value*1.3333, caht.value*1.6666],
+        datasets : [{
+            label : 'Marge sur Coût Variable',
+            backgroundColor : 'green',
+            borderColor : 'green',
+            borderWidth : 5,
+            data : ['0',mscv*0.3333, mscv*0.6666, mscv, mscv*1.3333, mscv*1.6666]
+        },
+        {
+            label : 'Coûts Fixes',
+            backgroundColor : 'red',
+            borderColor : 'red',
+            data : [ cfAmount.value, cfAmount.value, cfAmount.value, cfAmount.value, cfAmount.value, cfAmount.value]
+        }
+    ]
+    }
+    
+    var options = {
+        responsive : true,
+    };
+    
+    var config = {
+        type:'line',
+        data : data,
+        options : options,
+    }
+    var graphSr = new Chart(context, config);
+
 
 }
 
@@ -40,3 +85,4 @@ resetBtn.addEventListener('click', function(){
     benefitView.value = "";
     srView.value = "";
 })
+
